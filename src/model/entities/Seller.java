deb5f117621087ce57a075ee3c63248332cd90c5
VinyMT <viniciusmatiolatramontin@gmail.com>
2021-11-10 12:25:52 -0300

    Added seller class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,131 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	private Department department;	
+	
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+		this.setId(id);
+		this.setName(name);
+		this.setEmail(email);
+		this.setBirthDate(birthDate);
+		this.setBaseSalary(baseSalary);
+		this.setDepartment(department);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		if(id < 0 || id == null) {
+			throw new IllegalArgumentException("ID invalido");
+		}
+		
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if(name.isEmpty() || name == null) {
+			throw new IllegalArgumentException("Nome invalido");
+		}
+		
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		if(email.isEmpty() || email == null) {
+			throw new IllegalArgumentException("E-mail invalido");
+		}
+		
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		if(birthDate == null) {
+			throw new IllegalArgumentException("Data de nascimento invalida");
+		}
+		
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		if(baseSalary < 0|| baseSalary == null) {
+			throw new IllegalArgumentException("Salario base invalido");
+		}
+		
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		if(department == null) {
+			throw new IllegalArgumentException("Departamento invalido");
+		}
+		
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+	
+}
